18a85f144e0fca5d2a4bafb2cf21d5ebb868a767
Marvell <vmarvellinus@gmail.com>
2024-04-02 18:03:03 -0400

    Practicum 12

--- src/main/java/com/example/primesservice/model/Customer.java
+++ src/main/java/com/example/primesservice/model/Customer.java
@@ -1,11 +1,20 @@
 package com.example.primesservice.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Table
+@Entity
 public class Customer {
+    @Id
     private String username;
     private String password;
     public Customer(String username, String password) {
         this.username = username;
         this.password = password;
+    }
+    public Customer() {
     }
     public String getUsername() {
         return username;
